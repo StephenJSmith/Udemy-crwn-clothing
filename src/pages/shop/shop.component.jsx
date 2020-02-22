e9505acf50089935e7af5eedc299ced81ab8b365
Stephen Smith <stephensmith_aus@yahoo.com.au>
2020-02-22 17:27:56 +1100

    Lesson 123 Checout Item Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
 
-// import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 
 class ShopPage extends React.Component {
